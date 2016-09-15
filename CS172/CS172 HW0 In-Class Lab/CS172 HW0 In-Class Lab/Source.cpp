cf75fa53353f2ba956ef60592018286ec09adb9d
Shayna Snyder <ssnyder19@my.whitworth.edu>
2016-09-15 13:46:55 -0700

    added comments

--- CS172/CS172 HW0 In-Class Lab/CS172 HW0 In-Class Lab/Source.cpp
+++ CS172/CS172 HW0 In-Class Lab/CS172 HW0 In-Class Lab/Source.cpp
@@ -17,6 +17,7 @@ int main()
 	return 0;
 }
 
+//defines void function ex02
 void ex02()
 {
 	//declaring bool variable and initializing it to true
@@ -38,26 +39,38 @@ void ex02()
 	cout << endl << "Number of Shares: ";
 	//reads in user input into variable numberOfShares
 	cin >> numberOfShares;
+	//uses if statements to output whether or not variable numberOf Shares is less than 100
 	if (numberOfShares < 100)
 		cout << "numberOfShares is less than 100";
 	else
 		cout << "numberOfShares is not less than 100";
+	//prompts user to enter value for boxWidth
 	cout << endl << "Enter a box width: ";
+	//reads user input into int variable boxWidth
 	cin >> boxWidth;
+	//prompts user to enter value for bookWidth
 	cout << "Enter a book width: ";
+	//reads user input into int variable bookWidth
 	cin >> bookWidth;
+	//uses if statements and modulus function to output whether or not boxWidth is evenly divisible by bookWidth
 	if (boxWidth%bookWidth == 0)
 		cout << "The box width is evenly divisible by the book width.\n";
 	else
 		cout << "The box width is not evenly divisible by the book width.\n";
+	//prompts user to enter value for chocolateShelfLife
 	cout << "Enter the shelf life of a box of chocolate: ";
+	//reads user input into int variable chocolateShelfLife
 	cin >> chocolateShelfLife;
+	//prompts user to enter value for outsideTemp
 	cout << endl << "Enter the outside temperature: ";
+	//reads user input into int variable outsideTemp
 	cin >> outsideTemp;
+	//uses if statements to subtract 4 from the value of chocolateShelfLife if the outsideTemp is greater than 90
 	if (outsideTemp > 90)
 		chocolateShelfLife -= 4;
 }
 
+//defines void function ex03
 void ex03()
 {
 
